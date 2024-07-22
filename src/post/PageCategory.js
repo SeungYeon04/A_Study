9a0158e61e880c596e6e1bdb423f5b21eed0eba8
epsl.kwm@gmail.com <100332811+SeungYeon04@users.noreply.github.com>
2024-07-22 14:51:46 +0900

    .

--- src/post/PageCategory.js
+++ src/post/PageCategory.js
@@ -4,16 +4,16 @@ import { Link } from 'react-router-dom';
 
 const markdownFiles = [
   {
-    name: "카테고리1", type: "folder",
+    name: "CosProC", type: "folder",
     posts: [
-      { id: 1, name: "대학공부", type: "file" },
-      { id: 2, name: "유니티", type: "file" }
+      { id: 1, name: "가장많이받은선물-lv1", type: "file" },
+      { id: 2, name: "리눅스", type: "file" }
     ]
   },
   {
-    name: "카테고리2", type: "folder",
+    name: "리눅스", type: "folder",
     posts: [
-      { id: 3, name: "자바스크립트", type: "file" },
+      { id: 3, name: "2-2-리눅스일반-리눅스의이해", type: "file" },
       { id: 4, name: "리액트", type: "file" }
     ]
   }
