cc611dcd19aed56233b41ec17cb93337baa8d455
epsl.kwm@gmail.com <100332811+SeungYeon04@users.noreply.github.com>
2024-08-05 16:41:33 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,32 +1,30 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Category from './post/PageCategory';
-import Profile from './profile/profileImage'; 
-
+import Profile from './profile/profileImage';
 
 const rootContainer = document.getElementById('root');
 if (rootContainer) {
   const root = createRoot(rootContainer);
   root.render(
-      <BrowserRouter>
-        <App/>
-      </BrowserRouter>
+    <HashRouter>
+      <App />
+    </HashRouter>
   );
-} 
+}
 
 const categoryContainer = document.getElementById('Box2');
 if (categoryContainer) {
   const categoryRoot = createRoot(categoryContainer);
   categoryRoot.render(
-      <BrowserRouter>
-        <Category setSelectedCategory={() => {}}/>
-      </BrowserRouter>
+    <HashRouter>
+      <Category setSelectedCategory={() => {}} />
+    </HashRouter>
   );
-} 
-
+}
 
 const profileContainer = document.getElementById('profile');
 if (profileContainer) {
@@ -34,8 +32,6 @@ if (profileContainer) {
   profileRoot.render(
     <Profile />
   );
-} 
-
+}
 
 reportWebVitals();
-
