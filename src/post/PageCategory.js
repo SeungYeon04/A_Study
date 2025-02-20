71d23d2ff5de96a5f9fc1bf0979fb404d1d0993d
SeungYeon04 <100332811+SeungYeon04@users.noreply.github.com>
2025-02-20 19:17:41 +0900

    .

--- src/post/PageCategory.js
+++ src/post/PageCategory.js
@@ -16,10 +16,10 @@ const Category = ({ setSelectedCategory }) => {
       {markdownFiles.map((category, index) => (
         <div key={index}>
           {index === 0 && <h3>개인언어공부</h3>} 
-          {index === 3 && <h3>자격증공부</h3>} 
-          {index === 4 && <h3>프로젝트</h3>} 
-          {index === 5 && <h3>매일기록</h3>} 
-          {index === 6 && <h3>대학공부</h3>} 
+          {index === 4 && <h3>자격증공부</h3>} 
+          {index === 5 && <h3>프로젝트</h3>} 
+          {index === 6 && <h3>매일기록</h3>} 
+          {index === 7 && <h3>대학공부</h3>} 
           
           {category.type === 'folder' && (
               <Link to={`/${category.name}`} 
