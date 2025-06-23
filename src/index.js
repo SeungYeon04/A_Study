a0c059d47e3c82a6b1a11d648386d4311423a0c2
epsl.kwm@gmail.com <100332811+SeungYeon04@users.noreply.github.com>
2025-06-23 16:31:54 +0900

    .

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { HashRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Category from './post/PageCategory';
